ddd0d68e954015909dc83e14da891f0e54b05d3f
Developer Rifat <itscrifat5147@gmail.com>
2022-11-04 15:08:06 +0600

    cpr

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include <string>
-
-#include "cpr.h"
+#include "loading.cpp"
 
 using namespace std;
 
@@ -41,13 +40,6 @@ void mainMenu(){
 int main(int argc, char** argv) {
 //	loadingBar();
 //	mainMenu();
-	
-	cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
-                      cpr::Authentication{"user", "pass"},
-                      cpr::Parameters{{"anon", "true"}, {"key", "value"}});
-    r.status_code;                  // 200
-    r.header["content-type"];       // application/json; charset=utf-8
-    r.text; 
 		
 	return 0;
 }
